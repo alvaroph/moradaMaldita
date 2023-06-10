ab01e56bd8bea475ae1a79f015cbdf1f80578d7c
Alvaro <alvaroph@gmail.com>
2023-06-10 21:05:14 +0200

    empezado gema

--- src/views/TableroJuego.vue
+++ src/views/TableroJuego.vue
@@ -1,36 +1,40 @@
 <script setup lang="ts">
+import socket  from "../socket";
 import { reactive } from 'vue'
-/*import { onMounted } from 'vue'
+import { onMounted } from 'vue'
 
 onMounted(() => {
-  state.cartaActiva=-1;
-  state.cartas=[Carta{id:0, activa: false, img:"una.png", nVertices:[3] },
-            {id:1, activa: false, img:"dos.png", nVertices:[3] },
-            {id:2, activa: false , img:"tres.png", nVertices:[3] },
-            {id:3, activa: false , img:"cuatro.png", nVertices:[3] },
-            {id:4, activa: false , img:"cinco.png", nVertices:[3] }]
-    });*/
+  socket.on("envioCartas", (data) => {
+      console.log(data)
+      state.cartas=data.content;
+    });
+  });
 
 
  interface Carta {
     id: number,
+    num: number,
     activa: boolean,
     img: string,
     nVertices: number[]
   }
 
-   interface Mazo{
-    cartaActiva: number,
-    cartas: Carta[]
+  interface Gema{
+    id: number,
+    img:string,
+    libre: boolean
   }
 
-const state : Mazo = reactive({ 
+   interface Partida{
+    cartaActiva: number,
+    cartas: Carta[],
+    gemas: Gema[]
+  }
+
+const state : Partida = reactive({ 
     cartaActiva:     -1,
-    cartas: [{id:0, activa: false, img:"una.png", nVertices:[3] },
-            {id:1, activa: false, img:"dos.png", nVertices:[3] },
-            {id:2, activa: false , img:"tres.png", nVertices:[3] },
-            {id:3, activa: false , img:"cuatro.png", nVertices:[3] },
-            {id:4, activa: false , img:"cinco.png", nVertices:[3] }]
+    cartas: [],
+    gemas:[]
 });
   
   function volteaCarta(id : number){
@@ -39,6 +43,21 @@ const state : Mazo = reactive({
           state.cartaActiva++
       }
   }
+
+ function recibirCartas(){
+  console.log("dame cartas");
+  console.log(socket.id)
+  socket.emit("recibirCartas", {
+          userAsking: socket.id
+        });
+ 
+ }
+
+ 
+
+ function escogerGema(id: number){
+console.log(id)
+ }
 
 
 </script>
@@ -49,11 +68,16 @@ const state : Mazo = reactive({
     <h3>
       Bienvenido a la Morada Maldita
     </h3>
-    <v-btn>
-  Button
-</v-btn>
+
+    <span v-bind:key="gema.id" v-for="gema in state.gemas">
+        <img @click="escogerGema(gema.id)" class="carta" :src="gema.libre ? gema.img : '' " alt="">
+    </span>
+
+    <v-btn @click="recibirCartas">
+      Button
+    </v-btn>
     <span v-bind:key="carta.id" v-for="carta in state.cartas">
-        <img @click="volteaCarta(carta.id)" class="carta" :src="carta.activa ? carta.img : 'back.png'" alt="">
+        <img @click="volteaCarta(carta.num)" class="carta" :src="carta.activa ? carta.img : 'reverso.png' " alt="">
     </span>
   </div>
 </template>
